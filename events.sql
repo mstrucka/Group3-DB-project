a8f30b8c57dee5c780fd715255d88d5b4100fe17
Sam <sam.horacek@gmail.com>
2022-03-09 14:07:13 +0100

    Added events

---
+++ events.sql
@@ -0,0 +1,19 @@
+use mydb;
+delimiter $$;
+
+CREATE EVENT IF NOT EXISTS removePlatformSaleEvent
+ON SCHEDULE EVERY 1 DAY STARTS (CURRENT_DATE + INTERVAL 1 DAY + INTERVAL 1 HOUR)
+DO begin 
+    declare courseId int;
+    select course_id into courseId from coursesoftheday order by date DESC limit 1;
+	update courses
+	join coursesoftheday on coursesoftheday.course_id=courses.id
+	set platform_sale = 0
+	where courses.id=courseId;
+end
+
+CREATE EVENT IF NOT EXISTS selectRandomCourse
+ON SCHEDULE EVERY 1 DAY STARTS (CURRENT_DATE + INTERVAL 1 DAY + INTERVAL 1 HOUR)
+DO
+	insert into coursesoftheday (course_id, date)
+	select id, CURRENT_DATE from courses order by rand() limit 1;
