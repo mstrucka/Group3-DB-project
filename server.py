445797140205e9f96ebb82e2dbd59e563bad2b6e
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-03-19 15:41:28 +0100

    moved sql stuff to new folder

---
+++ server.py
@@ -0,0 +1,11 @@
+import os
+from bottle import route, run
+
+@route('/')
+def index():
+    return 'working'
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
