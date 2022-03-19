5cbf6365b86f34499ca49a52286912b35ef67a91
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-03-19 16:55:53 +0100

    shit works

--- db/mysql.py
+++ db/mysql.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine, text
 engine = create_engine('mysql+mysqldb://root:root@localhost/mydb', echo=True, future=True)
 
 with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
+    result = conn.execute(text("select * from users"))
     print(result.all())
