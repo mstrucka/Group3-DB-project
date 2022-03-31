7d04d97a471c24f630fc585ea66bf97b5aeb2875
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-03-31 14:35:45 +0200

    added user class

--- db/sql.py
+++ db/sql.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from db.models import Base
 
 # MySQL connection opened, global Session exposed
-engine = create_engine('mysql+mysqldb://root:root@localhost/mydb', echo=True, future=True)
+engine = create_engine('mysql+mysqldb://root:root@localhost/dbfordevs', echo=True, future=True)
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(engine)
