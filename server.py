b0faccf2c2e45dcfb11c47480edb853c9bd6f3f8
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-07 21:38:48 +0200

    added auto server reload for localhost

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ from db.sql.user import User
 def index():
     #u = User(firstName='John', lastName='Hitler', email='j@h.reich', password='sfjsd98sdfj', dob='1995-12-12', isStudent=True)
     with Session() as session:
-        u = session.query(User).filter_by(firstName='John').one()
+        u = session.query(User).filter(User.email.like('%reich%')).one()
     return { 'result': u.to_dict() }
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    run(host='localhost', port=8080, debug=True)
+    run(host='localhost', port=8080, debug=True, reloader=True)
