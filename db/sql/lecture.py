4c2c6639823d3654e2218f1d752560f567397d83
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-07 20:40:52 +0200

    smallinterger does not take argument

--- db/sql/lecture.py
+++ db/sql/lecture.py
@@ -6,8 +6,7 @@ class Lecture(Base, MyMixin, SerializerMixin):
     title = Column(String(80), nullable=False)
     description = Column(String(250))
     # could also be mysql.TINYINT
-    index = Column(SmallInteger(2), nullable=False)
+    index = Column(SmallInteger, nullable=False)
     
-
     def __repr__(self) -> str:
         return f'''<Lecture(id={self.id}, title={self.title}, index={self.index}>'''
--- db/sql/sql.py
+++ db/sql/sql.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.sql.models import Base
-from . import user, course, lecture, payment, resource
+#from . import user, course, lecture, payment, resource
 
 # MySQL connection opened, global Session exposed
 engine = create_engine('mysql+mysqldb://root:root@localhost/dbfordevs', echo=True, future=True)
