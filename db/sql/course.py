b5ee765918abb96c42e56b869918983352f40e23
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-08 16:43:47 +0200

    added indexes to course

--- db/sql/course.py
+++ db/sql/course.py
@@ -1,17 +1,21 @@
 from . models import MyMixin, Base
-from sqlalchemy import Column, Integer, String, Boolean, Numeric, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Numeric, ForeignKey, Index
 from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 
 class Course(MyMixin, SerializerMixin, Base):
     title = Column(String(45), nullable=False)
-    description = Column(String(150), nullable=False)
+    description = Column(String(500), nullable=False)
     level = Column(Integer)
     price = Column(Numeric(7, 2), nullable=False)
     platform_sale = Column(Boolean)
     category = Column(String(45))
     lecturer_id = Column(Integer, ForeignKey('user.id'))
     lecturer = relationship('User')
+
+    Index('course_title_price_idx', title.desc(), price.desc())
+    Index('course_category_idx', category.asc())
+    Index('lecturer_id_idx', lecturer_id.asc())
 
     def __repr__(self) -> str:
         return f'''<Course(id={self.id}, title={self.title}>'''
