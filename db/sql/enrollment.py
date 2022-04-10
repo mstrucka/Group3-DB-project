87866877be367a62bc4a7308833d6de21de39534
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-10 16:35:23 +0200

    cascading enrollment when deleting course

--- db/sql/enrollment.py
+++ db/sql/enrollment.py
@@ -6,12 +6,12 @@ from sqlalchemy_serializer import SerializerMixin
 
 class Enrollment(Base, MyMixin, SerializerMixin):
     student_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    #course_id = Column(Integer, ForeignKey('course.id'), nullable=False, index=True)
+    course_id = Column(Integer, ForeignKey('course.id', ondelete='CASCADE'), nullable=False, index=True)
     payment_id = Column(Integer, ForeignKey('payment.id'), nullable=False, index=True)
     finished = Column(Boolean)
 
     student = relationship('User')
-    #course = relationship('Course')
+    course = relationship('Course', cascade='all, delete')
     payment = relationship('Payment')
 
 
