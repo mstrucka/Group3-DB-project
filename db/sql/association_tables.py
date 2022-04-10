c8ce0a41a43e6ce713ecfdf9b6267802c752032b
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-10 16:22:42 +0200

    association tables

---
+++ db/sql/association_tables.py
@@ -0,0 +1,13 @@
+from sqlalchemy import ForeignKey, Column, Table, Index
+from . models import Base
+
+course_lectures = Table('course_lectures', Base.metadata,
+    Column('course_id', ForeignKey('course.id'), primary_key=True, index=True),
+    Column('lecture_id', ForeignKey('lecture.id'), primary_key=True, index=True),
+)
+
+lecture_resources = Table('lecture_resources', Base.metadata,
+    Column('lecture_id', ForeignKey('lecture.id'), primary_key=True, index=True),
+    Column('resource_id', ForeignKey('resource.id'), primary_key=True, index=True),
+)
+
