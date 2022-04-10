0f34a1cb08ab3aee0f018255fc2377cb7d2c5eac
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-04-10 16:21:55 +0200

    moved app starting point to app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import server
+
+import logging
+logging.basicConfig(
+    level=logging.DEBUG, 
+    filename='app.log', 
+    filemode='a+', 
+    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+)
+
+if __name__ == '__main__':
+    server.start_server()
+
