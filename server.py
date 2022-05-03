1ef35d9910fef8b2e73ae17c6a0a387e7cc0c28b
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-05-03 20:20:50 +0200

    debug mode

--- server.py
+++ server.py
@@ -9,6 +9,6 @@ def index():
 
 def start_server():    
     if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
     else:
         run(host='localhost', port=8080, debug=True, reloader=True)
