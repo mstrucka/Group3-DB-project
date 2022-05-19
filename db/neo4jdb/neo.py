f175fdc518aa3c1172b6dd508468d4bb35430cd3
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-05-19 22:36:02 +0200

    global db connection
    changed the protocol from neo4j to bolt in the uri, seems to work...

--- db/neo4jdb/neo.py
+++ db/neo4jdb/neo.py
@@ -1,33 +1,15 @@
 from neo4j import GraphDatabase
 import os
-
 from py2neo import Graph
 
+uri = os.getenv('NEO4J_URI')
+user = os.getenv("NEO4J_NAME")
+password = os.getenv("NEO4J_PASS")
+driver = GraphDatabase.driver(uri, auth=(user, password))
+graph = Graph(uri, auth=(user, password), secure=True)
 
-class App:
-    # uri = os.getenv("NEO4J_URI")
-    uri = "neo4j+s://956e17de.databases.neo4j.io"
-    user = os.getenv("NEO4J_NAME")
-    password = os.getenv("NEO4J_PASS")
-    graph = Graph(uri, auth=(user, password))
-
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-    def close(self):
-        # Don't forget to close the driver connection when you are finished with it
-        self.driver.close()
-
-    def create_nodes_and_relationships(self):
-        with self.driver.session() as session:
-            with open('creation_script.txt') as file:
-                query = file.read()
-                session.execute(query)
-
-
-# if __name__ == "__main__":
-#     uri = os.getenv("NEO4J_URI")
-#     user = os.getenv("NEO4J_NAME")
-#     password = os.getenv("NEO4J_PASS")
-#     app = App(uri, user, password)
-#     app.close()
+def create_nodes_and_relationships(driver):
+    with driver.session() as session:
+        with open('creation_script.txt') as file:
+            query = file.read()
+            session.execute(query)
