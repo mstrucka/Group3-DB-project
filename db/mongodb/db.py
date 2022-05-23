333c832f9f219013c4139e3113f7e3f214f64dce
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-05-23 17:30:07 +0200

    use local db when running local

--- db/mongodb/db.py
+++ db/mongodb/db.py
@@ -1,6 +1,11 @@
 import motor.motor_asyncio
 import os
-MONGO_DETAILS = os.getenv('MONGO_URI')
+MONGO_DETAILS = ''
+if os.environ.get('APP_LOCATION') == 'heroku':
+    MONGO_DETAILS = os.getenv('MONGO_URI')
+else:
+    MONGO_DETAILS = os.getenv('MONGO_URI_LOCAL')
+    
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client['learning-platform']
 
@@ -9,3 +14,5 @@ enrollment_collection = database.get_collection('enrollments')
 course_collection = database.get_collection('courses')
 course_of_the_day_collection = database.get_collection('courses_of_the_day')
 payment_collection = database.get_collection('payments')
+students_view = database.get_collection('students')
+lecturers_view = database.get_collection('lecturers')
